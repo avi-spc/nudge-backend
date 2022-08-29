a5867d16ff583bc9182d69722101ce100953568e
Avdhesh Yadav <avdhesh7394vn@gmail.com>
2022-08-29 22:54:47 +0530

    feat: created server, connected to mongo database, created api routes :sparkles:

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const ConnectDB = require('./config/db');
+
+const app = express();
+
+ConnectDB();
+
+app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Server running on port: ${PORT}`);
+});
