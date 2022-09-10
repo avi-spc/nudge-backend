961ad1cc659eec737073f29aedaf1c17f1551f81
Avdhesh Yadavm <avdhesh7394vn@gmail.com>
2022-09-10 20:38:14 +0530

    refactor: removed unnecessary code files :recycle:

--- server.js
+++ server.js
@@ -13,7 +13,6 @@ ConnectDB();
 app.get('/', (req, res) => res.send('API running'));
 
 app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/image', require('./routes/api/image'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
