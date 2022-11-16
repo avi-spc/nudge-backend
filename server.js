0d50bc8713c28689ae52d93b9a85af6b15c13e68
Avdhesh Yadavm <avdhesh7394vn@gmail.com>
2022-11-16 21:50:54 +0530

    added support for static files

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const methodOverride = require('method-override');
+const path = require('path');
 
 const { ConnectDB } = require('./config/db');
 
@@ -17,6 +18,15 @@ app.use('/api/notifications', require('./routes/api/notifications'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/users', require('./routes/api/users'));
+
+if (process.env.NODE_ENV === 'production') {
+	// Set a static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 
